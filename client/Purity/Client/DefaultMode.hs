c9a6b253aa910ac9e3040067e2e89dcedb85c113
Lazersmoke <lazersmoke@gmail.com>
2016-08-09 19:11:11 -0500

    Added Default Mode

---
+++ client/Purity/Client/DefaultMode.hs
@@ -0,0 +1,18 @@
+module Purity.Client.DefaultMode where
+
+import Purity.Client.Data
+import Purity.Client.Util
+
+import qualified Graphics.UI.GLFW as GLFW
+
+defaultKeyboardCallback :: GLFW.KeyCallback
+defaultKeyboardCallback _ _ _ _ _ = plog Warn "Default Keyboard Callback Invoked!"
+
+defaultMouseButtonCallback :: GLFW.MouseButtonCallback
+defaultMouseButtonCallback _ _ _ _ = plog Warn "Default Mouse Button Callback Invoked!"
+
+defaultRenderCallback :: GLFW.Window -> IO ()
+defaultRenderCallback _ = plog Warn "Default Render Callback Invoked!"
+
+defaultMode :: Mode
+defaultMode = buildMode defaultRenderCallback defaultKeyboardCallback defaultMouseButtonCallback
